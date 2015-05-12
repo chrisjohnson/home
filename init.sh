fcc017e603607bdc3d65276c1796082eb2bc6386
Chris Johnson <chris.johnson@alliedhealthmedia.com>
2015-05-12 12:59:23 -0400

    Better error checking for init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-cd ~/.vim/bundle/command-t/ruby/command-t
-ruby extconf.rb
-make
+command -v ruby >/dev/null 2>&1 || { echo >&2 "I require ruby but it's not installed.  Aborting."; exit 1; }
+command -v make >/dev/null 2>&1 || { echo >&2 "I require make but it's not installed.  Aborting."; exit 1; }
+command -v npm >/dev/null 2>&1 || { echo >&2 "I require npm but it's not installed.  Aborting."; exit 1; }
 
-cd ~/.vim/bundles/tern_for_vim.git
-npm install
+cd ~/.vim/bundle/command-t/ruby/command-t &&
+	ruby extconf.rb &&
+	make
+
+cd ~/.vim/bundle/tern_for_vim.git &&
+	npm install
