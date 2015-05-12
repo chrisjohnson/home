ba6157ec0e0c234d6e42959d679d3de30d67c57f
Chris Johnson <chris.johnson@alliedhealthmedia.com>
2015-05-12 13:00:34 -0400

    Also initialize/update submodules

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git submodule update --init --recursive
+
 command -v ruby >/dev/null 2>&1 || { echo >&2 "I require ruby but it's not installed.  Aborting."; exit 1; }
 command -v make >/dev/null 2>&1 || { echo >&2 "I require make but it's not installed.  Aborting."; exit 1; }
 command -v npm >/dev/null 2>&1 || { echo >&2 "I require npm but it's not installed.  Aborting."; exit 1; }
