0b7f063feee19f9d589396d3ec4a01d68195f5ca
Chris Johnson <cjohnson@covermymeds.com>
2017-08-07 17:00:42 -0400

    Clean up old junk

--- init.sh
+++ init.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-SCRIPTPATH=`dirname $0`
-
 git submodule update --init --recursive
-
-command -v ruby >/dev/null 2>&1 || { echo >&2 "I require ruby but it's not installed.  Aborting."; exit 1; }
-command -v make >/dev/null 2>&1 || { echo >&2 "I require make but it's not installed.  Aborting."; exit 1; }
-command -v npm >/dev/null 2>&1 || { echo >&2 "I require npm but it's not installed.  Aborting."; exit 1; }
-
-cd ~/.vim/bundle/command-t/ruby/command-t && if [ ! -e Makefile ]; then
-	ruby extconf.rb &&
-	make
-fi
-
-echo "Done!"
