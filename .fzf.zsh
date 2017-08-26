a5ab570b131f64afb3798d4aa5c77dea27fb434d
Chris Johnson <chrisjohnson0@gmail.com>
2017-08-26 04:44:09 -0400

    Handle fzf config in a more portable way

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,18 +1,21 @@
-# Setup fzf
-# ---------
-if [[ "$FZF_BASE_PATH" == "" ]]; then
-	export FZF_BASE_PATH="/usr/local/opt/fzf"
+# Setup fzf (handle a
+if [[ -a "/usr/local/opt/fzf" ]]; then
+	FZF_SHELL_PATH="/usr/local/opt/fzf/shell"
+	FZF_BIN_PATH="/usr/local/opt/fzf/bin"
+elif [[ -a "/usr/share/fzf" ]]; then
+	FZF_SHELL_PATH="/usr/share/fzf"
+else
+	# Could not locate fzf install, nothing to do here
+	return
 fi
 
-if [[ ! "$PATH" == *"$FZF_BASE_PATH/bin"* ]]; then
-  export PATH="$PATH:$FZF_BASE_PATH/bin"
-fi
+# $PATH
+# If fzf is not already found in $PATH, check to see if it's because $FZF_BASE_PATH/bin is missing from $PATH, and add it if so
+type fzf &>/dev/null || { "$PATH" != *"$FZF_BIN_PATH"* && export PATH="$PATH:$FZF_BIN_PATH" ; }
 
 # Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "$FZF_BASE_PATH/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$FZF_SHELL_PATH/completion.zsh" 2> /dev/null
 
 # Key bindings
-# ------------
-source "$FZF_BASE_PATH/shell/key-bindings.zsh"
+source "$FZF_SHELL_PATH/key-bindings.zsh"
 
